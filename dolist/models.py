0b7d01790e42107af33faa177d6ca0b76278081a
FredyPV05 <158099336+FredyPV05@users.noreply.github.com>
2025-12-01 06:03:56 +0000

    update

--- dolist/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Todolist(models.Model):
-    text = models.CharField(max_length=100)
-    completed = models.BooleanField(default=False)
--- lab15_django_new/dolist/forms.py
+++ lab15_django_new/dolist/forms.py
@@ -3,7 +3,7 @@ from django import forms
 class Todolistform(forms.Form):
     text = forms.CharField(max_length=100, widget=forms.TextInput(
         attrs={
-            'class' : 'form-control',
+            'class' : 'todo_text',
             'placeholder' : 'Enter to do...',
             'aria-label' : 'Todo',
             'aria-describeby' : 'add-btn'
