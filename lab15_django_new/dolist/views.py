c7a6c0210ac15ad1d235b21e04c2a4acfa09ca5e
FredyPV05 <158099336+FredyPV05@users.noreply.github.com>
2025-12-01 05:43:08 +0000

    update

---
+++ lab15_django_new/dolist/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .models import Todolist
+from .forms import Todolisform
+from django.views.decorators.http import require_POST
+
+# Create your views here.
+def index(request):
+    todo_tasks = Todolist.objects.order_by('id')
+    #form = Todolistform()
+    context = {'todo_tasks': todo_tasks}
+    return render(request, 'index.html', context)
+
+@require_POST
+def addTodoitem(request):
+    form = Todolistform(request.POST)
+    print(request.POST['text']) # for testing
